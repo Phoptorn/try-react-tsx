ce33768eb33155d97e2e4133c18cfa4cf0ed742d
Phoptorn <phoptorn.khow@gmail.com>
2022-08-24 14:58:54 +0700

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+// https://medium.com/@thanatchakromsang/%E0%B8%A1%E0%B8%B2%E0%B8%88%E0%B8%B1%E0%B8%9A-react-%E0%B8%A2%E0%B8%B1%E0%B8%94%E0%B9%83%E0%B8%AA%E0%B9%88-container-%E0%B8%81%E0%B8%B1%E0%B8%99%E0%B9%80%E0%B8%96%E0%B8%AD%E0%B8%B0-react-docker-45ece149d603
+
 import React from 'react';
 import './App.css';
 import { Route, Switch } from 'react-router-dom';
@@ -7,12 +9,10 @@ import About from './About';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
         <Switch>
           <Route path='/about' component={About} />
           <Route path='/' component={Home} />
         </Switch>
-      </header>
     </div>
   );
 }
